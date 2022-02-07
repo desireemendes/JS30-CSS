5bfff74491e5f255e8dd5c94a30a2ad0f39f90ae
Desiree Mendes <desireefrost@live.ca>
2022-02-07 18:17:18 -0500

    add event listener, js functionality for inputs

---
+++ public/script.js
@@ -0,0 +1,13 @@
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+window.addEventListener("load", () => {
+  const inputs = document.querySelectorAll(".controls input");
+  inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+  inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
+});
